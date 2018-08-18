937827f41c9603d7a236c65469323f5fe3f5adbb
Yuanjianxin <1814888800@qq.com>
2018-08-18 19:00:45 +0800

    upgrade grpc-db-service-core version to 0.0.6

--- config/databaseConf.js
+++ config/databaseConf.js
@@ -15,6 +15,6 @@ module.exports = [
             min: 0, // 连接池中最小连接数量
             idle: 10000 // 如果一个线程 10 秒钟内没有被使用过的话，那么就释放线程
         },
-        operatorsAliases: false
+        operatorsAliases: true
     },
 ];
